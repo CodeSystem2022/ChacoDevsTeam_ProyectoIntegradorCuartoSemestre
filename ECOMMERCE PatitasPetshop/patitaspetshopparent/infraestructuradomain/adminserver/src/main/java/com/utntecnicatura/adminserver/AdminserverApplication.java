0f890bf32c81b2262ba89b128a5c2fe1a534b646
Agustin Gutierrez <yo_agustingutierrez@hotmail.com>
2023-09-23 21:44:35 -0300

    1.4.1-BACKEND INFRAESTRUCTURA-Creacion de Microservicio Admin Server#8

---
+++ ECOMMERCE PatitasPetshop/patitaspetshopparent/infraestructuradomain/adminserver/src/main/java/com/utntecnicatura/adminserver/AdminserverApplication.java
@@ -0,0 +1,107 @@
+package com.utntecnicatura.adminserver;
+
+import ch.qos.logback.classic.Logger;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import static io.netty.handler.codec.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+@EnableAdminServer
+@SpringBootApplication
+@EnableWebSecurity
+@EnableAutoConfiguration(exclude = ReactiveManagementWebSecurityAutoConfiguration.class)
+@Configuration
+public class AdminserverApplication {
+
+	private static final String[] AUTH_WHITELIST = {
+			"/swagger-resources",
+			"/swagger-resources/**",
+			"/configuration/ui",
+			"/configuration/security",
+			"/swagger-ui.html",
+			"/webjars/**",
+			"/v3/api-docs/**",
+			"/api/public/**",
+			"/api/public/authenticate",
+			"/actuator/*",
+			"/swagger-ui/**"
+	};
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminserverApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("http://192.168.0.229:3000"); // Cambia esto a la URL de tu aplicación React en producción
+		config.addAllowedHeader("http://localhost:3000");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://172.22.112.1:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.cors(cors -> cors.disable());
+		return http.build();
+	}
+
+
+	@Bean
+	HttpClient httpClient(){
+		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
+}
---
+++ ECOMMERCE PatitasPetshop/patitaspetshopparent/infraestructuradomain/adminserver/src/test/java/com/utntecnicatura/adminserver/AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utntecnicatura.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
