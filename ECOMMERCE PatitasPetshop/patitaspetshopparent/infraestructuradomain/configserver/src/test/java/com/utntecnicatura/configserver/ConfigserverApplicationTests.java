6345f9a984067f03dfbf805d246790ee8ccd11f1
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-09-26 21:20:37 -0300

    Cambios en el MSC (#14)

---
+++ ECOMMERCE PatitasPetshop/patitaspetshopparent/infraestructuradomain/configserver/src/test/java/com/utntecnicatura/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utntecnicatura.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
